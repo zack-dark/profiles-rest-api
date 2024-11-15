b6ad4838e48faae5b68911ccb001902f74ab2566
zack dark <zikas.dark2019@gmail.com>
2024-11-16 00:07:55 +0100

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -12,6 +12,7 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError('User must have an email address')
 
+
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
@@ -41,7 +42,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Retrieve full name of user"""
